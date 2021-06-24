20819bfc6464c59491e2ec38944ff53109756c2c
Asmaa sanad <asmaasanasd4@gmail.com>
2021-06-24 12:22:21 +0200

    Done Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import LoginForm from './components/LoginForm';
 
 function App() {
+  const [user, setUser] = useState({ email: '', password: '' });
+
+  const Login = details => {
+    setUser({
+      email: details.email,
+      password: details.password
+    })
+  };
+
+  const LogoutHandler = () => {
+    setUser({ email: '', password: '' });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-
-        <h1>Login</h1>
-        <LoginForm />
+        {(user.email) ? (
+          <div>
+            <h2>Welcome, <span>{user.email}</span></h2>
+            <button onClick={LogoutHandler}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <h1>Login</h1>
+            <LoginForm Login={Login} />
+          </div>)
+        }
       </header>
     </div>
   );
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,7 +3,7 @@ import './Login.css';
 
 const LoginForm = (props) => {
     const [form, setForm] = useState({ email: '', password: '' });
-    const [data, setData] = useState(false);
+    // const [data, setData] = useState(false);
 
     const onChangeHandler = (e) => {
         setForm(prevState => ({
@@ -13,17 +13,17 @@ const LoginForm = (props) => {
     };
     const onClickhandler = () => {
         if (form.password && form.email) {
-            setData(true);
+            // setData(true);
         }
     };
     const submitHandler = () => {
-
+        props.Login(form);
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <div className="con">
-                <div>
+        <form onSubmit={submitHandler} className="con">
+            <div >
+                <div className="inp">
                     <label htmlFor="email">Email: </label>
                     <input type="email" id="email" name="email" onChange={onChangeHandler} value={form.email || ''} />
                 </div>
@@ -32,13 +32,13 @@ const LoginForm = (props) => {
                     <input type="password" id="password" name="password" onChange={onChangeHandler} value={form.password || ''} />
                 </div>
                 <button onClick={onClickhandler} >Login</button>
-                <input type="submit" value="SUBMIT" />
+                {/* <input type="submit" value="SUBMIT" />
                 {data ? <div>
                     <p>
                         {form.email}
                     </p>
                     <p> {form.password}</p>
-                </div> : <div> No Data</div>}
+                </div> : <div> No Data</div>} */}
             </div>
         </form>
     );
