e712f3be7301e564eca74bcb61953d4d076a8e37
Asmaa sanad <asmaasanasd4@gmail.com>
2021-06-24 09:27:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Login</h1>
+        <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const LoginForm = (props) => {
+    const [form, setForm] = useState({ email: '', password: '' });
+    const [data, setData] = useState(false);
+
+    const onChangeHandler = (e) => {
+        setForm(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+    const onClickhandler = () => {
+        if (form.password && form.email) {
+            setData(true);
+        }
+    };
+    const submitHandler = () => {
+
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="con">
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" name="email" onChange={onChangeHandler} value={form.email || ''} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password" onChange={onChangeHandler} value={form.password || ''} />
+                </div>
+                <button onClick={onClickhandler} >Login</button>
+                <input type="submit" value="SUBMIT" />
+                {data ? <div>
+                    <p>
+                        {form.email}
+                    </p>
+                    <p> {form.password}</p>
+                </div> : <div> No Data</div>}
+            </div>
+        </form>
+    );
+}
+
+export default LoginForm;
