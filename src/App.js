ea58d6909a7f9e81eefb509cb8c1046034e6f89d
Asmaa sanad <asmaasanasd4@gmail.com>
2021-06-24 16:35:57 +0200

    Done Login

--- src/App.js
+++ src/App.js
@@ -4,32 +4,43 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 
 function App() {
+  const admin = {email: 'asmaa@as.com', password: '1234'}
   const [user, setUser] = useState({ email: '', password: '' });
+  const [error,setError]= useState(true);
 
   const Login = details => {
-    setUser({
-      email: details.email,
-      password: details.password
-    })
+    if(details.email=== admin.email&& details.password=== admin.password){
+      setError(false);
+      setUser({
+        email: details.email,
+        password: details.password
+      });
+    }else{
+      setError(true);
+    }
   };
 
   const LogoutHandler = () => {
+    setError(true);
     setUser({ email: '', password: '' });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        {(user.email) ? (
+        {(error) ? (
+          <div>
+          <h1>Login</h1>
+          <LoginForm Login={Login} />
+          {(error)? <div>error </div> :<div></div>}
+        </div>
+          
+        ) : ( 
           <div>
             <h2>Welcome, <span>{user.email}</span></h2>
             <button onClick={LogoutHandler}>Logout</button>
           </div>
-        ) : (
-          <div>
-            <h1>Login</h1>
-            <LoginForm Login={Login} />
-          </div>)
+          )
         }
       </header>
     </div>
